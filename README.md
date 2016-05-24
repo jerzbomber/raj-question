675fef311f39070b3f9d58fa88879a2ffb4316c3
Toby Jackson <tobyjackson@Tobys-MBP.fios-router.home>
2016-05-24 03:21:43 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Raj Sample Solution
+
+This project is one way to solve the following:
+
+*Given an array of transactions, want to return similar array
+with some added info. Eliminates the need to call functions
+more than once in map and makes use of the object spread option
+(Stage 2 proposal for ECMAScript but currently being used in production
+in projects such as React Native). You must use a transpiler in order
+to use it so I am using Babel to transpile my project.*
+
+## Installing
+
+```sh
+npm install
+```
+
+## Building
+
+```sh
+npm run build
+```
+
+## Documentation
+
+Pay special attention to the following files:
+
+* **package.json** - devDependencies section
+* **.babelrc** - note the transform-object-rest-spread plugin
+* **example.js** - read through the comments
---
+++ example.js
@@ -0,0 +1,75 @@
+// sample transactions with base properties
+const transactions = [
+  {
+    id: 1,
+    startPrice: 95.87,
+    endPrice: 96.43,
+    qty: 100
+  },
+  {
+    id: 2,
+    startPrice: 717.91,
+    endPrice: 717.24,
+    qty: 50
+  },
+  {
+    id: 3,
+    startPrice: 125.96,
+    endPrice: 125.15,
+    qty: 200
+  }
+]
+
+// synchronously finds the symbol
+const findSymbol = (id) => {
+  switch (id) {
+    case 1:
+      return 'AAPL'
+    case 2:
+      return 'GOOGL'
+    case 3:
+      return 'LNKD'
+  }
+}
+
+// calculates the cost basis
+const calcCostBasis = (startPrice, qty) => {
+  return startPrice / qty
+}
+
+// calculates the market value
+const calcMktVal = (endPrice, qty) => {
+  return endPrice *  qty
+}
+
+// Fun stuff
+// Array.map() takes in a function whose first parameter
+// is the current value (object) so you can just memoize
+// the values inside of the function body and then re-use.
+
+// Also, if you are using Babel as a transpiler, there is a
+// plugin named babel-plugin-transform-object-rest-spread
+// which will allow you to use object spread syntax. This is
+// more concise than _.default({},{},{}) or Object.assign({},{},{}).
+
+// You can see it in action in the return statement.
+// Here I'm using the spread operator to copy enumerable properties
+// from one object to another in a more succinct way.
+let newTransactions = transactions.map(t => {
+  // let's memoize the calculated values for later use
+  let costBasis = calcCostBasis(t.startPrice, t.qty)
+  let mktVal = calcMktVal(t.endPrice, t.qty)
+
+  // use the object spread operator to create a new object
+  // and add additional properties
+  return {
+    ...t,
+    ticker: findSymbol(t.id),
+    costBasis: costBasis,
+    mktVal: mktVal,
+    gainLoss: mktVal - costBasis
+  }
+})
+
+// output results to console (pretty-printing is set using the third arg)
+console.log('output', JSON.stringify(newTransactions, null, 2))
